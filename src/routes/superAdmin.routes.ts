098e873930bb699da6e31f4e898617fcc8c7cb3c
Sahil Hossain <sahilhossain999coc@gmail.com>
2025-09-03 22:16:09 +0530

    Themes

--- src/routes/superAdmin.routes.ts
+++ src/routes/superAdmin.routes.ts
@@ -3,14 +3,21 @@ import {authorizeRoles} from '../middlewares/role.middleware';
 import {protect} from '../middlewares/auth.middleware';
 import * as superAdminCtrl from '../controllers/superadmin.controller';
 import {validate} from "../middlewares/validateRequest";
-import {addAdminSchema, getAdminDetailsSchema, saveChangesSchema} from "../validations/superadmin.validation";
+import {
+    addAdminSchema,
+    addThemeSchema,
+    getAdminDetailsSchema,
+    saveChangesSchema
+} from "../validations/superadmin.validation";
 
 const router = express.Router();
 
 router.use(protect); // JWT middleware
 router.use(authorizeRoles('super-admin'));
 router.get('/admins', superAdminCtrl.getAllAdmins);
+router.get('/themes', superAdminCtrl.getThemes);
 router.post('/add-admin', validate(addAdminSchema), superAdminCtrl.addAdmin);
+router.post('/add-theme', validate(addThemeSchema), superAdminCtrl.addTheme);
 router.post('/admin', validate(getAdminDetailsSchema), superAdminCtrl.getAdminDetails);
 router.post('/save-changes', validate(saveChangesSchema), superAdminCtrl.saveAdminChanges);
 
