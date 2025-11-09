cd83967190ab702c795f12d8571e4d238de0366f
Sahil Hossain <sahilhossain999coc@gmail.com>
2025-11-09 22:23:29 +0530

    V2 Login Apis

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -20,7 +20,7 @@ router.post('/super-admin/session-login', protect, superAdminSessionLogin);
 router.post('/admin/login', validate(loginSchema), adminLogin);
 router.post('/admin/session-login', protect, adminSessionLogin);
 
-router.post('/user/login', validate(loginSchema), userLogin);
-router.post('/user/session-login', protect, userSessionLogin);
+router.post('/user/login-2', validate(loginSchema), userLogin);
+router.post('/user/session-login-2', protect, userSessionLogin);
 
 export default router;
